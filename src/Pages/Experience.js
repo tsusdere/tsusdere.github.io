e84e378762466ed67c1ff0855c19753052560e95
tsusdere <fishesandanime@gmail.com>
2022-01-15 02:11:40 -0600

    fixed work

--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -7,20 +7,20 @@ function Experience() {
       <div className="intel">
         <img
           src="/intel.png"
-          style={{ width: "100px", height: "auto", float: "right" }}
+          style={{ width: "200px", height: "auto", float: "right" }}
         ></img>
-        <h1>Intel</h1>
+		  <h1> <u><b>Intel</b></u></h1>
         <em style={{ fontSize: "15px" }}>Aug. 2021 - Present</em>
         <h5>Graphics Software Engineer Intern</h5>
 
-        <Blink text=">To be continued..." fontSize="20px" color="blue"></Blink>
+        <Blink text=">To be continued..." fontSize="20px" color="dodgerBlue"></Blink>
       </div>
       <div className="fccu">
         <img
           src="/fccu.png"
-          style={{ width: "75px", height: "auto", float: "right" }}
+          style={{ width: "100px", height: "auto", float: "right" }}
         ></img>
-        <h3>First Community Credit Union</h3>{" "}
+		  <h3><u><b>First Community Credit Union</b></u></h3>{" "}
         <em style={{ fontSize: "15px" }}>May 2021 - Aug. 2021</em>
         <h5>Data Analyst</h5>
       </div>
@@ -28,9 +28,9 @@ function Experience() {
         {" "}
         <img
           src="/runner.png"
-          style={{ width: "125px", height: "auto", float: "right" }}
+          style={{ width: "200px", height: "auto", float: "right" }}
         ></img>
-        <h3>University of Texas at San Antonio</h3>{" "}
+		  <h3><u><b>University of Texas at San Antonio</b></u></h3>{" "}
         <em style={{ fontSize: "15px" }}>Sep. 2019 - May. 2021</em>
         <h5>Computer Science Tutor</h5>
         <em style={{ fontSize: "15px" }}>Sep. 2018 - Sep. 2019</em>
